32512ac6bd14c70e087390963b759931e9e7749b
unknown <maldinuribrahim@gmail.com>
2019-11-26 18:49:37 +0700

    Menu Authorization

--- resources/views/admin/v_1/templates/sidebar.blade.php
+++ resources/views/admin/v_1/templates/sidebar.blade.php
@@ -1,12 +1,14 @@
-<li class="m-menu__item  {{Route::current()->getName() == 'user' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
-    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\User\Http\Controllers\UserController@index')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-users"></i>
-        <span class="m-menu__link-title"> 
-            <span class="m-menu__link-wrap"> 
-                <span class="m-menu__link-text">
-                    User
-                </span>
+@can('menu-user')
+    <li class="m-menu__item  {{Route::current()->getName() == 'user' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
+        <a href="{{action('\Gdevilbat\SpardaCMS\Modules\User\Http\Controllers\UserController@index')}}" class="m-menu__link ">
+            <i class="m-menu__link-icon flaticon-users"></i>
+            <span class="m-menu__link-title"> 
+                <span class="m-menu__link-wrap"> 
+                    <span class="m-menu__link-text">
+                        User
+                    </span>
+                 </span>
              </span>
-         </span>
-     </a>
-</li>
+         </a>
+    </li>
+@endcan
