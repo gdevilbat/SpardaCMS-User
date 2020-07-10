f7eac60994caf1c33ad0a1e90643207d09e2a84e
maldinuribrahim <maldinuribrahim@gmail.com>
2020-07-11 05:47:19 +0700

    Fix User Role Icon

--- resources/views/admin/v_1/content/User/master.blade.php
+++ resources/views/admin/v_1/content/User/master.blade.php
@@ -79,7 +79,7 @@
                         <div class="col-md-5">
                             <a href="javascript:void(0)" class="btn btn-brand m-btn m-btn--custom m-btn--icon m-btn--pill m-btn--air" data-toggle="m-popover" title="" data-content="You're not Allowed To Take This Action. Pleas Ask Admin !!!" data-original-title="Forbidden Action">
                                 <span>
-                                    <i class="la la-plus"></i>
+                                    <i class="la la-ban"></i>
                                     <span>Add New User</span>
                                 </span>
                             </a>
