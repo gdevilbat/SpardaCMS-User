32d3a9f0f00d7cb4c3d63ea17e17294a02e2af56
maldinuribrahim <maldinuribrahim@gmail.com>
2020-09-12 12:57:51 +0700

    Add Rate Limit Table

---
+++ Database/Migrations/2020_09_12_055209_add_rate_limit_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRateLimitUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->decimal('rate_limit', 6, 0)->after('api_token')
+                        ->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'rate_limit'));
+            {
+                $table->dropColumn('rate_limit');
+            }
+        });
+    }
+}
